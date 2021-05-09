602b236837fa27a60979cec4bbbca80d2e6acb95
Puziichuk Olena <nonna467@gmail.com>
2021-05-09 17:59:54 +0300

    added tasks

--- index2.js
+++ index2.js
@@ -5,24 +5,28 @@ const users = [
   { name: 'Lux', active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
-  const updatedUsers = allUsers.map(user =>
-    user.name === userName ? { ...user, active: !user.active } : user,
-  );
+const toggleUserState = (allUsers, userName) => {
+    return new Promise((resolve) => {
+    
+        const updatedUsers = allUsers.map(user =>
+            user.name === userName ? { ...user, active: !user.active } : user,
+        );
 
-  callback(updatedUsers);
+        resolve(updatedUsers);
+        
+    });
 };
 
 const logger = updatedUsers => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-toggleUserState(users, 'Mango', logger);
-toggleUserState(users, 'Lux', logger);
-
-/*
- * Должно работать так
+// /*
+//  * Сейчас работает так
 //  */
-// toggleUserState(users, 'Mango').then(logger);
-// toggleUserState(users, 'Lux').then(logger);
+// // toggleUserState(users, 'Mango', logger);
+// // toggleUserState(users, 'Lux', logger);
+
+// /*
+//  * Должно работать так
+//  */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
